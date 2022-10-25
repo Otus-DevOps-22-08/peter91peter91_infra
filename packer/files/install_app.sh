28526ece6d3d6ce1f135ac88b9fe14f3e33c71b8
Petr Moiseev <peter__91@mail.ru>
2022-10-25 10:33:17 +0000

    Deleted packer and terraform. Had an error with GIT

--- packer/files/install_app.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-sudo apt install -y git
-cd /opt && git clone -b monolith https://github.com/express42/reddit.git
-cd reddit && bundle install
--- packer/scripts/install_ruby.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-sudo apt update
-sleep 1m
-sudo apt install -y ruby-full ruby-bundler build-essential
-echo "apt updated and ruby installed."
